8c6af842c4084a012bc562c27cde85fb9ca41e8c
Maksim Evtukh <maksevtukh@gmail.com>
2021-09-03 11:14:16 +0300

    upd table module

---
+++ examples/ExampleApp/ExampleApp/SearchLocale.swift
@@ -0,0 +1,13 @@
+//
+//  SearchLocale.swift
+//  ExampleApp
+//
+//  Created by Максим Евтух on 03.09.2021.
+//  Copyright © 2021 DreamTeam Apps. All rights reserved.
+//
+
+import Foundation
+
+enum SearchLocale: String {
+    case selectionFormatter = "search.selectionFormatter"
+}
